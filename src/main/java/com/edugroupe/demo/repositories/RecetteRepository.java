f8298434d0d8003b05ff527fafd796db68ef9394
JOUBERT <pierre-louis.joubert@capgemini.com>
2019-10-15 16:50:44 +0200

    Travail en cour: fonction InsertTestData

--- src/main/java/com/edugroupe/demo/repositories/RecetteRepository.java
+++ src/main/java/com/edugroupe/demo/repositories/RecetteRepository.java
@@ -1,9 +1,11 @@
 package com.edugroupe.demo.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.edugroupe.demo.metiers.Recette;
 
+@RepositoryRestResource
 public interface RecetteRepository extends PagingAndSortingRepository<Recette, Integer> {
 
 }
