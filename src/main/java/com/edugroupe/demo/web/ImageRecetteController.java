57b182d37cb9ab9ee6a240b66eb8d255e62ec98c
Joubertp <pierrejoubert883@gmail.com>
2020-01-31 15:53:20 +0100

    Achèvement gestion images pour recettes
    Sans les thumbnails toutefois

--- src/main/java/com/edugroupe/demo/web/ImageRecetteController.java
+++ src/main/java/com/edugroupe/demo/web/ImageRecetteController.java
@@ -1,7 +1,13 @@
 package com.edugroupe.demo.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.edugroupe.demo.metiers.ImageRecette;
+import com.edugroupe.demo.metiers.Recette;
 import com.edugroupe.demo.repositories.ImageRecetteRepository;
 import com.edugroupe.demo.repositories.RecetteRepository;
 
@@ -34,14 +41,55 @@ public class ImageRecetteController {
 	@CrossOrigin("http://localhost:4200")		
 	public ResponseEntity<ImageRecette> upload(@RequestParam("file") MultipartFile file,
 											   @RequestParam("recetteId") int recetteId){
-		return null;
+		Optional<Recette> recette = recetteRepository.findById(recetteId);
+		if(!recette.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		ImageRecette image = new ImageRecette(0,
+										file.getOriginalFilename(),
+										file.getContentType(),
+										"");
+		
+		try {
+			imageRecetteRepository.saveImageFile(image, file.getInputStream());
+			image.setRecette(recette.get());
+			image = imageRecetteRepository.save(image);
+			return new ResponseEntity<ImageRecette>(image, HttpStatus.CREATED);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		
 	}
 	
 	@GetMapping(value = "/{id:[0-9]+}/data")
 	@ResponseBody
 	@CrossOrigin("http://localhost:4200")
-	public ResponseEntity<FileSystemResource> imageData(@PathVariable("id")int id) {
+	public ResponseEntity<FileSystemResource> imageData(@PathVariable("id") int id) {
 		
-		return null;
+		Optional<ImageRecette> oimage = imageRecetteRepository.findById(id);
+		if(!oimage.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		Optional<File> ofile = imageRecetteRepository.getImageFile(oimage.get().getStorageId());
+		if(!ofile.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.parseMediaType(oimage.get().getContentType()));
+		headers.setContentLength(ofile.get().length());
+		headers.setContentDispositionFormData("attachment", oimage.get().getFilename());
+		
+		ResponseEntity<FileSystemResource> re = new ResponseEntity<FileSystemResource>(
+					new FileSystemResource(ofile.get()),
+					headers,
+					HttpStatus.OK
+				);
+		
+		return re;		
 	}
 }
