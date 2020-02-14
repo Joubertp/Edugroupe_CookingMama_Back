d9ed1511dd004a5286523ceab990bd609648d6e6
Unknown <unknown@example.com>
2020-02-14 16:47:15 +0100

    Creation des entites metier Fournisseur et IngredientFournisseur
    Et ajout d'une méthode pour récupérer les infos d'un ingrédient

---
+++ src/main/java/com/edugroupe/demo/metiers/Fournisseur.java
@@ -0,0 +1,29 @@
+package com.edugroupe.demo.metiers;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Fournisseur {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String libelle;
+	@JsonIgnore
+	@OneToMany(mappedBy = "fournisseur")
+	private Set<IngredientFournisseur> ingredientsFournisseur;
+	
+}
---
+++ src/main/java/com/edugroupe/demo/metiers/IngredientFournisseur.java
@@ -0,0 +1,27 @@
+package com.edugroupe.demo.metiers;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class IngredientFournisseur {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private double prix;
+	private String infos;
+	@ManyToOne
+	private Fournisseur fournisseur;
+	@ManyToOne
+	private Ingredient ingredient;
+	
+}
