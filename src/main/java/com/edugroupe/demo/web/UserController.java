49aaf73856841e8f5843616ceab3d2e9897f1751
Joubertp <pierrejoubert883@gmail.com>
2020-02-14 10:54:07 +0100

    correction de merge foirreux

--- src/main/java/com/edugroupe/demo/web/UserController.java
+++ src/main/java/com/edugroupe/demo/web/UserController.java
@@ -1,6 +1,7 @@
 package com.edugroupe.demo.web;
 
 import java.security.Principal;
+import java.util.Optional;
 
 import javax.annotation.security.RolesAllowed;
 
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
